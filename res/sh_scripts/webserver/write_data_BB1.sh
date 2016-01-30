99e56a0a861f5f1a18d96808494f597daf2ae3d5
MarcosCM <marketes94@gmail.com>
2016-01-30 20:06:03 +0100

    Changed view

--- res/sh_scripts/webserver/write_data_BB1.sh
+++ res/sh_scripts/webserver/write_data_BB1.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+DATE=$(date +%T | sed 's/\([0-9]*\):\([0-9]*\):.*/\1:\2:00/')
 #Measure temperature
-echo $(date +%T | sed 's/\([0-9]*\):\([0-9]*\):.*/\1:\2:00/') $(cat /sys/bus/i2c/devices/i2c-1/1-0040/temp1_input) >> /www/pages/res/WC1/temperature.txt
+echo $DATE $(cat /sys/bus/i2c/devices/i2c-1/1-0040/temp1_input) >> /www/pages/res/WC1/temperature.txt
 tail -n 30 /www/pages/res/WC1/temperature.txt > /www/pages/res/WC1/temperature_tmp.txt; mv /www/pages/res/WC1/temperature_tmp.txt /www/pages/res/WC1/temperature.txt
 #Measure pressure
-echo $(date +%T | sed 's/\([0-9]*\):\([0-9]*\):.*/\1:\2:00/') $(cat /sys/bus/i2c/devices/i2c-1/1-0077/pressure_input) >> /www/pages/res/WC1/pressure.txt
+echo $DATE $(cat /sys/bus/i2c/devices/i2c-1/1-0077/pressure_input) >> /www/pages/res/WC1/pressure.txt
 tail -n 30 /www/pages/res/WC1/pressure.txt > /www/pages/res/WC1/pressure_tmp.txt; mv /www/pages/res/WC1/pressure_tmp.txt /www/pages/res/WC1/pressure.txt
 #Measure humidity
-echo $(date +%T | sed 's/\([0-9]*\):\([0-9]*\):.*/\1:\2:00/') $(cat /sys/bus/i2c/devices/i2c-1/1-0040/humidity1_input) >> /www/pages/res/WC1/humidity.txt
+echo $DATE $(cat /sys/bus/i2c/devices/i2c-1/1-0040/humidity1_input) >> /www/pages/res/WC1/humidity.txt
 tail -n 30 /www/pages/res/WC1/humidity.txt > /www/pages/res/WC1/humidity_tmp.txt; mv /www/pages/res/WC1/humidity_tmp.txt /www/pages/res/WC1/humidity.txt
